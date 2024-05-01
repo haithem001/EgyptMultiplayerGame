e5aaf5a6446f501f3337197d9b8e4eea67185df6
onfire <haithemtrabelsino@gmail.com>
2024-05-01 17:27:15 +0100

    4inputs

--- src/INIT_SERVER.java
+++ src/INIT_SERVER.java
@@ -4,10 +4,13 @@ import java.awt.*;
 import java.awt.event.*;
 public class INIT_SERVER extends JFrame implements ActionListener {
     // JTextField
-    static JTextField t;
-
+    static JTextField IP=new JTextField(16);
+    static JTextField PORT=new JTextField(16);
+    static JTextField NICKNAME= new JTextField(16);
+    static JTextField PLAYER_ID= new JTextField(16);
     // JFrame
     static JFrame f = new JFrame("textfield");
+
 
     // JButton
     static JButton b= new JButton("submit");
@@ -17,13 +20,20 @@ public class INIT_SERVER extends JFrame implements ActionListener {
 
     // default constructor
      INIT_SERVER() {
+
      }
     public static void text(){
          INIT_SERVER te = new INIT_SERVER();
          b.addActionListener(te);
-         t= new JTextField(16);
+
          JPanel p = new JPanel();
-         p.add(t);
+         p.setLayout(new GridLayout(5,1,0,0));
+         p.setBackground(Color.black);
+         p.add(IP);
+         p.add(PORT);
+         p.add(NICKNAME);
+         p.add(PLAYER_ID);
+
          p.add(b);
          p.add(l);
          f.add(p);
@@ -45,14 +55,14 @@ public class INIT_SERVER extends JFrame implements ActionListener {
             // set the text of the label to the text of the field
             f.dispose();
 
-            l.setText(t.getText());
+            l.setText(IP.getText());
             LaunchGame server = new LaunchGame();
             server.setSize(this.getSize());
             server.setVisible(true);
             server.setBackground(Color.black);
 
             // set the text of field to blank
-            t.setText("  ");
+            IP.setText("  ");
         }
     }
 }
